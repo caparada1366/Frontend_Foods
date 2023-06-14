d38c56ea625ca0afcda6b50eafbe97b4e1e7762c
caparada1366 <ca.parada1366@gmail.com>
2023-06-14 17:59:00 -0500

    deploy

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Cards.css'
+import { Link } from 'react-router-dom'
+
+export default function Cards({image, name, diets, id, healthScore}) {
+  return (
+    <Link to={`/detail/${id}`}>
+    <div className='cards'>
+      <h2>Nombre: {name}</h2>
+      <img className='img' src={image} alt={name} style={{width: '17vw', height:'25vh'}}></img>
+      <h4>Dietas: {diets.join(", ")}</h4>
+      <h3>Health Score: {healthScore}</h3>
+    </div>
+    </Link>
+  )
+}
